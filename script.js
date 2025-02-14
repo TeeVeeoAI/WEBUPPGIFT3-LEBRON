ba85d550711e83e3ac5d99766f7a24ac48847379
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2025-02-14 13:48:40 +0100

    +++

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function ToggleThemeL(){
     document.getElementById('måne').style.display = "block";
     document.getElementById('sol').style.display = "none";
 }
+
+function FormCheck(){
+    if (document.getElementById('TAC').checked){
+        document.getElementById('form').removeAttribute('disabled');
+    }
+    else {
+        document.getElementById('form').setAttribute('disabled', true)
+    }
+}
+let score = 0;
+let add = 1;
+
+function LebronClicker(){
+    if(score == 10){
+        add++;
+    }
+    score += add;
+    document.getElementById('score').innerHTML = "score: " + score;
+    if(score == 100){
+        alert('You win!!!!');
+    }
+    if(score == 1000){
+        alert('You extra win!!!! (Sponsore by Jetmir)')
+    }
+}
