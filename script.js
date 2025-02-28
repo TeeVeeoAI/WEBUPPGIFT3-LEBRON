aae4151ecd8103a7ce928689a99027b8428e3a2f
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2025-02-28 13:48:39 +0100

    +++

--- script.js
+++ script.js
@@ -19,19 +19,48 @@ function FormCheck(){
         document.getElementById('form').setAttribute('disabled', true)
     }
 }
+
+//LeClicker
 let score = 0;
 let add = 1;
+let multiply = 1;
+let price = [10, 100];
 
 function LebronClicker(){
-    if(score == 10){
-        add++;
-    }
-    score += add;
-    document.getElementById('score').innerHTML = "score: " + score;
+    score += add * multiply;
+    ScoreUpdate();
     if(score == 100){
         alert('You win!!!!');
     }
     if(score == 1000){
         alert('You extra win!!!! (Sponsore by Jetmir)')
     }
+}
+
+function OnePlus(){
+    if (score >= price[0]){
+        add++;
+        score -= price[0];
+        price[0]++;
+    }
+    ScoreUpdate();
+    PriceUpdate();
+}
+
+function MultiplyerPointOne(){
+    if (score >= price[1]){
+        multiply += 0.1;
+        score -= price[1];
+        price[1] += 10;
+    }
+    ScoreUpdate();
+    PriceUpdate();
+}
+
+function ScoreUpdate(){
+    document.getElementById('score').innerHTML = "score: " + Math.floor(score*10)/10;
+}
+
+function PriceUpdate(){
+    document.getElementById('onePlus').innerHTML = price[0] + pre
 }
